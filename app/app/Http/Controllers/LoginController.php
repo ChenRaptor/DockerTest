475b8476d5b51ce370dedba4b8b34623186638b2
Chen <ChenRaptor@outlook.fr>
2023-09-19 17:29:56 +0200

    base

--- app/app/Http/Controllers/LoginController.php
+++ app/app/Http/Controllers/LoginController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Password;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 use Validator;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class LoginController extends Controller
 {
@@ -21,16 +25,39 @@ class LoginController extends Controller
         ]);
      
         if ($validated->fails()) {
-            return redirect('/login')
+            return redirect('/form')
                 ->withErrors($validated)
                 ->withInput();
         }
+
+
+        Password::create([
+            'site' => $request->url,
+            'login' => $request->email,
+            'password' => Hash::make($request->password),
+            'user_id' => Auth::user()->id
+        ]);
+
+        // $table->id();
+        // $table->string('site', 255);
+        // $table->string('login', 255);
+        // $table->longText('password');
+        // $table->bigInteger('user_id');
+        // $table->timestampsTz()->nullable();
      
         $jsonData = json_encode($request->post());
         $current_timestamp = Carbon::now()->timestamp;
         $filePath = "{$current_timestamp}.json";
         Storage::put($filePath, $jsonData);
         
-        return redirect('/login');
+        return redirect('/form');
+    }
+
+    public function show(Request $request): RedirectResponse
+    {
+
+        $users = DB::table('passwords')->where('user_id', Auth::user()->id)->get();
+ 
+        return redirect('/showpassword')->with('status', 'Profile updated!');
     }
 }
--- app/routes/web.php
+++ app/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,8 +19,21 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/login', function () {
-    return view('login');
-})->name('login');
+Route::get('/form', function () {
+    return view('form');
+});
 
-Route::post('/logincontroller', [LoginController::class, 'store'])->name('LoginController');
+Route::get('/showpassword', [LoginController::class, 'show']);
+Route::post('/logincontroller', [LoginController::class, 'store']);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
