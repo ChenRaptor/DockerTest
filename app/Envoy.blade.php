500b1987e864dd8a921010efc5c7c04a72e8502c
chen <chenraptor@outlook.fr>
2024-02-08 17:17:24 +0100

    Envoy #2

--- app/Envoy.blade.php
+++ app/Envoy.blade.php
@@ -7,16 +7,19 @@
 @endstory
 
 @task('install-dependencies', ['on' => ['web']])
+    cd /home/antoine-bonneau/antoine-bonneau.dhonnabhain.me/app
     composer install
     npm i
     npm run build
 @endtask
 
 @task('migrate', ['on' => ['web']])
-    php artisan migrate
+    cd /home/antoine-bonneau/antoine-bonneau.dhonnabhain.me/app
+    php artisan migrate --force
 @endtask
 
 @task('optimize', ['on' => ['web']])
+    cd /home/antoine-bonneau/antoine-bonneau.dhonnabhain.me/app
     composer install --optimize-autoloader --no-dev
     php artisan optimize:clear
     php artisan optimize
